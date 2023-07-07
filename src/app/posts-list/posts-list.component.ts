2bb11168841c19e68b262ac47f62fd88e6bc0289
nina <ninagbs@abv.bg>
2023-07-07 16:59:19 +0300

    lists all themes and posts

--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Post } from '../types/post';
 
 @Component({
   selector: 'app-posts-list',
@@ -7,11 +8,16 @@ import { ApiService } from '../api.service';
   styleUrls: ['./posts-list.component.css']
 })
 export class PostsListComponent implements OnInit{
+  postList:Post[]=[];
 constructor(private apiService: ApiService){}
 
 ngOnInit(): void {
+  
   // this is observable and we have to subscribe 
-  this.apiService.getPosts(5).subscribe((posts)=>console.log({posts}))
+  this.apiService.getPosts(5).subscribe((posts)=>{
+    console.log(posts[0])
+    this.postList=posts;
+  })
 }
 
 
