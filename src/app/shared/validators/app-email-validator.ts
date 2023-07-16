5755ae49e689fcd96037cbacab9c26c1693d71e2
nina <ninagbs@abv.bg>
2023-07-16 18:29:13 +0300

    correctin about email validation

--- src/app/shared/validators/app-email-validator.ts
+++ src/app/shared/validators/app-email-validator.ts
@@ -1,12 +1,14 @@
 import { ValidatorFn } from "@angular/forms";
 
 export function appEmailValidator(domains: string[]): ValidatorFn {
-    // [^@]{6,}@gmail\.(bg|com)$
-    const domainStrings = domains.join('|');
-    const regExp = new RegExp(`[^@]{6,}@gmail\.(${domainStrings})$`)
-    return (control) =>{
-        return control.value=='' ||
-        regExp.test(control.value)
-        ? null : {appEmailValidator: true}
-    }
+  const domainStrings = domains.join("|"); // ['bg', 'com'] => bg|com
+  const regExp = new RegExp(`[^@]{6,}@gmail\.(${domainStrings})$`);
+
+  return (control) => {
+    const x = control.value;
+    const z = regExp.test(control.value);
+    return control.value === "" || regExp.test(control.value)
+      ? null
+      : { appEmailValidator: true };
+  };
 }
