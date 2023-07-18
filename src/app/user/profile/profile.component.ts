f0b9aa1f301ff299704a2042f8d181bae6152404
nina <ninagbs@abv.bg>
2023-07-18 15:46:33 +0300

    profile component corrections

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { DEFAULT_EMAIL_DOMAINS } from 'src/app/shared/constants';
+import { appEmailValidator } from 'src/app/shared/validators/app-email-validator';
 
 @Component({
   selector: 'app-profile',
@@ -9,15 +11,18 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class ProfileComponent {
 isEditMode: boolean =false;
 form = this.fb.group({
-  username: ['', Validators.required],
-  email:['',],
-  tel:['tel1']
+  username: ['', Validators.required, Validators.minLength(5)],
+  email:['', Validators.required, appEmailValidator(DEFAULT_EMAIL_DOMAINS)],
+  tel:['']
 })
 constructor(private fb: FormBuilder){}
 toggleEditMode(): void{
   this.isEditMode=!this.isEditMode;
 }
 saveProfileHandler():void{
+ if( this.form.invalid){
+  return;
+ }
   this.toggleEditMode()
 }
 }
